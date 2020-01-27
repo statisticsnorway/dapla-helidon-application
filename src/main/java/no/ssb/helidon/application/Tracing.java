50b49c16d947391357006e54a896bd2dc908bde7
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-01-27 07:31:51 +0100

    Move Tracing utility to common application module

---
+++ src/main/java/no/ssb/helidon/application/Tracing.java
@@ -0,0 +1,67 @@
+package no.ssb.helidon.application;
+
+import com.google.protobuf.MessageOrBuilder;
+import io.helidon.common.context.Contexts;
+import io.helidon.webserver.ServerRequest;
+import io.opentracing.Span;
+import io.opentracing.SpanContext;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+
+public class Tracing {
+
+    public static <T extends MessageOrBuilder> T traceInputMessage(Span span, T message) {
+        span.log(Map.of("event", "debug-input", "data", message.toString()));
+        traceInputMessage(span, message.toString());
+        return message;
+    }
+
+    public static <T extends MessageOrBuilder> T traceOutputMessage(Span span, T message) {
+        traceOutputMessage(span, message.toString());
+        return message;
+    }
+
+    public static void traceInputMessage(Span span, String message) {
+        span.log(Map.of("event", "debug-input", "data", message));
+    }
+
+    public static void traceOutputMessage(Span span, String message) {
+        span.log(Map.of("event", "debug-output", "data", message));
+    }
+
+    public static void logError(Span span, Throwable e, String event) {
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        span.log(Map.of("event", event, "message", e.getMessage(), "stacktrace", stringWriter.toString()));
+    }
+
+    public static <T extends MessageOrBuilder> Span spanFromGrpc(T message, String operationName) {
+        SpanContext spanContext = Contexts.context()
+                .get()
+                .get(SpanContext.class)
+                .get();
+        Tracer tracer = GlobalTracer.get();
+        Span span = tracer
+                .buildSpan(operationName)
+                .asChildOf(spanContext)
+                .start();
+        tracer.scopeManager().activate(span);
+        traceInputMessage(span, message);
+        return span;
+    }
+
+    public static Span spanFromHttp(ServerRequest request, String operationName) {
+        SpanContext spanContext = request.spanContext();
+        Tracer tracer = request.tracer();
+        Span span = tracer
+                .buildSpan(operationName)
+                .asChildOf(spanContext)
+                .start();
+        tracer.scopeManager().activate(span);
+        return span;
+    }
+}

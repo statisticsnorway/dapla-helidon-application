f30e03baa4fcd978af43f677c50380ca099a7ef4
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-11-24 10:04:35 +0100

    More flexible default config with profile support

--- src/main/java/no/ssb/helidon/application/DefaultHelidonApplicationBuilder.java
+++ src/main/java/no/ssb/helidon/application/DefaultHelidonApplicationBuilder.java
@@ -4,24 +4,33 @@ import io.helidon.config.Config;
 
 import static io.helidon.config.ConfigSources.classpath;
 import static io.helidon.config.ConfigSources.file;
+import static java.util.Optional.ofNullable;
 
 public abstract class DefaultHelidonApplicationBuilder implements HelidonApplicationBuilder {
 
     public static Config createDefaultConfig() {
-        String overrideFile = System.getenv("HELIDON_CONFIG_FILE");
         Config.Builder builder = Config.builder();
+        String overrideFile = ofNullable(System.getProperty("helidon.config.file"))
+                .orElseGet(() -> System.getenv("HELIDON_CONFIG_FILE"));
         if (overrideFile != null) {
             builder.addSource(file(overrideFile).optional());
         }
-        return builder
-                .addSource(file("conf/application.yaml").optional())
-                .addSource(classpath("application.yaml"))
-                .build();
+        String profile = ofNullable(System.getProperty("helidon.config.profile"))
+                .orElseGet(() -> System.getenv("HELIDON_CONFIG_PROFILE"));
+        if (profile != null) {
+            String profileFilename = String.format("application-%s.yaml", profile);
+            builder.addSource(file(profileFilename).optional());
+            builder.addSource(classpath(profileFilename).optional());
+        }
+        builder.addSource(file("conf/application.yaml").optional());
+        builder.addSource(classpath("application.yaml"));
+        return builder.build();
     }
 
     protected Config config;
 
     protected DefaultHelidonApplicationBuilder() {
+        config = createDefaultConfig();
     }
 
     @Override
